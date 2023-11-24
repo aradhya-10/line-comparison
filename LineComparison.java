671176f01e3c13bfae6976342558c2c8c23be4a4
aradhya-10 <202051034@iiitvadodara.ac.in>
2023-11-24 14:59:02 +0530

    UC1: Distance betnween two points

--- LineComparison.java
+++ LineComparison.java
@@ -1,10 +1,22 @@
 import java.util.*;
+import java.text.*;
 
 public class LineComparison {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to Line Comparison Computation Program on Main Branch");
+		
+		System.out.println("Enter the coordinates for 1st Point: ");
+		int x1 = sc.nextInt();
+		int y1 = sc.nextInt();
+		System.out.println("Enter the coordinates for 2nd Point: ");
+		int x2 = sc.nextInt();
+		int y2 = sc.nextInt();
+
+		double distance = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+		DecimalFormat fmt = new DecimalFormat("#.00");
+		System.out.println("Distance Between the points = "+ fmt.format(distance));
 		sc.close();
 	}
 }
