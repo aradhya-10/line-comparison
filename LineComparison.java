850f1c5dece528b84c5a0174680b942b7a218f6b
aradhya-10 <202051034@iiitvadodara.ac.in>
2023-11-24 14:46:26 +0530

    Initial Commit

---
+++ LineComparison.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to Line Comparison Computation Program on Main Branch");
+		sc.close();
+	}
+}
