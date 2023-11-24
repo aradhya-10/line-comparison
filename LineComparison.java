18be52a0e60ccbcd2670f116b014b64568bd14b8
aradhya-10 <202051034@iiitvadodara.ac.in>
2023-11-24 15:09:35 +0530

    UC2: Check if two line are equal

--- LineComparison.java
+++ LineComparison.java
@@ -7,16 +7,31 @@ public class LineComparison {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to Line Comparison Computation Program on Main Branch");
 		
-		System.out.println("Enter the coordinates for 1st Point: ");
+		System.out.println("Enter the coordinates for 1st Point(x1,y2) for the first line: ");
 		int x1 = sc.nextInt();
 		int y1 = sc.nextInt();
-		System.out.println("Enter the coordinates for 2nd Point: ");
+		System.out.println("Enter the coordinates for 2nd Point(x2,y2) or the first line: ");
 		int x2 = sc.nextInt();
 		int y2 = sc.nextInt();
-
-		double distance = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
-		DecimalFormat fmt = new DecimalFormat("#.00");
-		System.out.println("Distance Between the points = "+ fmt.format(distance));
+		
+		System.out.println("Enter the coordinates for 1st Point(x1,y2) for the second line: ");
+		int a1 = sc.nextInt();
+		int b1 = sc.nextInt();
+		System.out.println("Enter the coordinates for 2nd Point(x2,y2) or the second line: ");
+		int a2 = sc.nextInt();
+		int b2 = sc.nextInt();
 		sc.close();
+
+		double distance1 = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+		double distance2 = Math.sqrt(Math.pow((a2-a1), 2) + Math.pow((b2-b1), 2));
+
+		DecimalFormat fmt = new DecimalFormat("#.00");
+		System.out.println("Length of First Line = "+ fmt.format(distance1));
+		System.out.println("Length of Second Line = "+ fmt.format(distance2));
+		
+		if(Double.toString(distance2).equals(Double.toString(distance1)))
+			System.out.println("Lengths are equal.");
+		else
+			System.out.println("Lengths are not equal.");
 	}
 }
